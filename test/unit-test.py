3be449d6f544ab5f0b873890b835edf33d869461
Emrah T <33878173+EmrhT@users.noreply.github.com>
2023-03-20 23:39:59 +0300

    Update unit-test.py

--- test/unit-test.py
+++ test/unit-test.py
@@ -1,4 +1,15 @@
-from unittest.mock import Mock
+import unittest
 
-thing = Mock(return_value=0)
-return thing
+class Testing(unittest.TestCase):
+    def test_string(self):
+        a = 'some'
+        b = 'some'
+        self.assertEqual(a, b)
+
+    def test_boolean(self):
+        a = True
+        b = True
+        self.assertEqual(a, b)
+
+if __name__ == '__main__':
+    unittest.main()
